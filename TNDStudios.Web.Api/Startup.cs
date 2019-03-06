4507c361c89632192c759634ed555ac7df9b3f28
TNDStudios <info@thenakeddeveloper.com>
2019-03-06 01:22:36 +0000

    Added Swagger / Swashbuckle / Swagger UI

--- TNDStudios.Web.Api/Startup.cs
+++ TNDStudios.Web.Api/Startup.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using TNDStudios.Web.ApiManager.Security.Authentication;
-using TNDStudios.Web.ApiManager.Security;
-using Microsoft.AspNetCore.Mvc.Versioning;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
 using System.IO;
+using TNDStudios.Web.Api.Documentation;
+using TNDStudios.Web.ApiManager.Security;
+using TNDStudios.Web.ApiManager.Security.Authentication;
 
 namespace TNDStudios.Web.Api
 {
@@ -36,12 +39,43 @@ namespace TNDStudios.Web.Api
 
             // Set up the Api versioning
             services
-                .AddApiVersioning(o =>
+                .AddVersionedApiExplorer(options =>
+                {
+                    options.GroupNameFormat = "'v'VVV";
+
+                    // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
+                    // can also be used to control the format of the API version in route templates
+                    options.SubstituteApiVersionInUrl = true;
+                });
+
+            services.AddApiVersioning(o =>
                 {
                     o.ApiVersionReader = new HeaderApiVersionReader("api-version");
                     o.AssumeDefaultVersionWhenUnspecified = true;
                     o.ReportApiVersions = true;
                 });
+
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(options =>
+            {
+                // resolve the IApiVersionDescriptionProvider service
+                // note: that we have to build a temporary service provider here because one has not been created yet
+                var provider = services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
+
+                // add a swagger document for each discovered API version
+                // note: you might choose to skip or document deprecated API versions differently
+                foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+                }
+
+                // add a custom operation filter which sets default values
+                options.OperationFilter<SwaggerDefaultValues>();
+
+                // integrate xml comments
+                //options.IncludeXmlComments(XmlCommentsFilePath);
+            });
 
             // Custom service setup for the API Manager
             services
@@ -50,7 +84,7 @@ namespace TNDStudios.Web.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApiVersionDescriptionProvider provider)
         {
             if (env.IsDevelopment())
             {
@@ -72,6 +106,40 @@ namespace TNDStudios.Web.Api
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(options =>
+            {
+                // build a swagger endpoint for each discovered API version
+                foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                }
+            });
+        }
+
+        static Info CreateInfoForApiVersion(ApiVersionDescription description)
+        {
+            var info = new Info()
+            {
+                Title = $"Salesforce Outbound API {description.ApiVersion}",
+                Version = description.ApiVersion.ToString(),
+                Description = "Salesforce integration with Swagger, Swashbuckle, Input Formatting and API versioning.",
+                Contact = new Contact() { Name = "Joe Walters", Email = "info@thenakeddeveloper.com" },
+                TermsOfService = "Owned By TNDStudios",
+                License = new License() { Name = "MIT", Url = "https://opensource.org/licenses/MIT" }
+            };
+
+            if (description.IsDeprecated)
+            {
+                info.Description += " <b color=\"red\">This API version has been deprecated</b>.";
+            }
+
+            return info;
         }
     }
 }
