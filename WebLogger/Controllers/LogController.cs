6346139a5082aca9858c9a1f17f2410192f60254
TNDStudios <info@thenakeddeveloper.com>
2019-03-17 23:47:11 +0000

    Set up multi project start and cross project logging from nlog

---
+++ WebLogger/Controllers/LogController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebLogger.Controllers
+{
+    [Route("api/logger")]
+    public class LogController
+    {
+        [Http]
+        public void Get()
+        {
+
+        }
+    }
+}
