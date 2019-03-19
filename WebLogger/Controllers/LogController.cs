4511ab737579af59f7845ae06db21600586108c5
TNDStudios <info@thenakeddeveloper.com>
2019-03-19 13:26:58 +0000

    Split of logging and metrics to seperate controllers

---
+++ WebLogger/Controllers/LogController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebLogger.Controllers
+{
+    /// <summary>
+    /// The payload from the log service
+    /// </summary>
+    public class LogPayload
+    {
+        public string Message { get; set; }
+        public string Level { get; set; }
+    }
+
+    [Route("api/log")]
+    [ApiController]
+    public class LogController : Controller
+    {
+        public LogController()
+        {
+
+        }
+
+        [HttpPost]
+        public void Post([FromForm] LogPayload payload)
+        {
+            // Analyse the payload to confirm it really is not a metric type
+            if (!payload.Message.StartsWith("metric:"))
+            {
+                Debug.WriteLine($"{payload.Level} - {payload.Message}");
+            }
+        }
+    }
+}
